f9b9d16cbbb2b31b6e8869198b277e2c1fff8283
tomosyan <45392239@qq.com>
2025-05-27 10:33:54 +0800

    Update utils.py

--- INLONG2025052101(UI)/utils.py
+++ INLONG2025052101(UI)/utils.py
@@ -12,7 +12,7 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with Printrun.  If not, see <http://www.gnu.org/licenses/>.
-
+#
 import os
 import platform
 import sys
